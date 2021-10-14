098480fe213f093c53f6f3d1b0409246c638c816
gabguerin <gabriel.guerin.a@gmail.com>
2021-10-15 00:49:22 +0200

    add data augmentation & update dtw

--- extract_landmarks.py
+++ extract_landmarks.py
@@ -3,42 +3,79 @@ import os
 import numpy as np
 import mediapipe as mp
 from utils import mediapipe_detection, save_array
+from scipy.linalg import expm, norm
 
 
-def flatten_landmarks(landmark_list, s, e):
-    if landmark_list:
-        l = []
-        for landmark in landmark_list.landmark[s:e]:
-            l += [landmark.x, landmark.y, landmark.z]
-        return l
-    return [np.nan for i in range((e - s) * 3)] # Return zero matrix if the landmark doesn't exist
+def landmark_to_array(landmark_list):
+    keypoints = []
+    for landmark in landmark_list.landmark:
+        keypoints.append([landmark.x, landmark.y, landmark.z])
+    return np.array(keypoints)
+
+
+def translate_keypoints(keypoints: np.array, idx: int, target_v: np.array):
+    """
+    Function that will centered all keypoints
+        to better compare hand movement for example
+    :param keypoints: list of keypoints
+    :param idx: idx of the keypoint which will be moved
+    :return: list of keypoints centered
+    """
+    if np.sum(keypoints) == 0:
+        return keypoints
+
+    kp0 = keypoints[idx]
+    for i in range(keypoints.shape[0]):
+        keypoints[i, 0] -= kp0[0] - target_v[0]
+        keypoints[i, 1] -= kp0[1] - target_v[1]
+        keypoints[i, 2] -= kp0[2] - target_v[2]
+    return keypoints
+
+
+def rotate_keypoints(keypoints: np.array, axis: np.array, angle: float):
+    rot_M = expm(np.cross(np.eye(3), axis / norm(axis) * angle))
+    return np.dot(rot_M, keypoints.T).T
+
 
 def extract_keypoints(results):
-    standardize_results(results)
+    """
+    Transform results in a list of standardized keypoints to be able to compute dtw distances
+    :param results: mediapipe object that contains the 3D position of all keypoints
+    :return: A tuple containing hand landmarks or NaN if a hand doesn't appear in the results
+    """
+    # this list contains keypoints of the left and right hand
+    hands = [False, False]
 
-    #pose = flatten_landmarks(results.pose_landmarks, 9, 18)
-    lh = flatten_landmarks(results.left_hand_landmarks, 0, 21)
-    rh = flatten_landmarks(results.right_hand_landmarks, 0, 21)
-    return lh + rh
+    pose = landmark_to_array(results.pose_landmarks)
+    axis = np.array((0, 1, (pose[11, 2] + pose[12, 2]) / 2))
 
-
-def standardize_results(results):
     if results.left_hand_landmarks:
-        left_wrist = results.left_hand_landmarks.landmark[0]
-        for landmark in results.left_hand_landmarks.landmark:
-            landmark.x -= left_wrist.x - 0.5
-            landmark.y -= left_wrist.y - 0.5
-            landmark.z -= left_wrist.z
+        lh1 = landmark_to_array(results.left_hand_landmarks)
+
+        lh2 = rotate_keypoints(lh1, axis, 0.05)
+        lh2 = translate_keypoints(lh2, 0, pose[15])
+
+        lh3 = rotate_keypoints(lh1, axis, -0.05)
+        lh3 = translate_keypoints(lh3, 0, pose[15])
+
+        hands[0] = lh1.reshape(63).tolist() + lh2.reshape(63).tolist() + lh3.reshape(63).tolist()
+
     if results.right_hand_landmarks:
-        right_wrist = results.right_hand_landmarks.landmark[0]
-        for landmark in results.right_hand_landmarks.landmark:
-            landmark.x -= right_wrist.x - 0.5
-            landmark.y -= right_wrist.y - 0.5
-            landmark.z -= right_wrist.z
+        rh1 = landmark_to_array(results.right_hand_landmarks)
+
+        rh2 = rotate_keypoints(rh1, axis, 0.05)
+        rh2 = translate_keypoints(rh2, 0, pose[15])
+
+        rh3 = rotate_keypoints(rh1, axis, -0.05)
+        rh3 = translate_keypoints(rh3, 0, pose[15])
+
+        hands[1] = rh1.reshape(63).tolist() + rh2.reshape(63).tolist() + rh3.reshape(63).tolist()
+    return hands
 
 
 def extract_landmarks(video, save=True):
-    keypoint_list = []
+    lh_list = []
+    rh_list = []
 
     cap = cv2.VideoCapture(os.path.join("Videos",video))
     # Set mediapipe model
@@ -50,8 +87,9 @@ def extract_landmarks(video, save=True):
                 image, results = mediapipe_detection(frame, holistic)
 
                 # Store results
-                keypoint_list.append(extract_keypoints(results))
-
+                lh, rh = extract_keypoints(results)
+                lh_list.append(lh)
+                rh_list.append(rh)
             else:
                 break
         cap.release()
@@ -61,4 +99,4 @@ def extract_landmarks(video, save=True):
         # Saving landmarks
         fname = video.split('.')[0] if '.' in video else video
         path = os.path.join("landmarks", fname + ".pickle")
-        save_array(np.nan_to_num(keypoint_list), path)
+        save_array([lh_list, rh_list], path)
