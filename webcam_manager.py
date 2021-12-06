f539dc620a1373d398ab4dfb015f55fd73b3a247
Gabriel GUERIN <gabriel.guerin@sicara.com>
2021-12-06 12:07:09 +0100

    Update WebcamManager.draw_landmarks

--- webcam_manager.py
+++ webcam_manager.py
@@ -85,32 +85,37 @@ class WebcamManager(object):
         # Draw pose connections
         mp_drawing.draw_landmarks(
             image,
-            pose_connexions,
-            mp_holistic.POSE_CONNECTIONS,
-            mp_drawing.DrawingSpec(color=(235, 52, 86), thickness=2, circle_radius=4),
-            mp_drawing.DrawingSpec(color=(52, 235, 103), thickness=2, circle_radius=2),
+            landmark_list=results.pose_landmarks,
+            connections=pose_connexions,
+            landmark_drawing_spec=mp_drawing.DrawingSpec(
+                color=(235, 52, 86), thickness=2, circle_radius=4
+            ),
+            connection_drawing_spec=mp_drawing.DrawingSpec(
+                color=(52, 235, 103), thickness=2, circle_radius=2
+            ),
         )
         # Draw left hand connections
         mp_drawing.draw_landmarks(
-                image,
-                landmark_list=results.left_hand_landmarks,
-                connections=mp_holistic.HAND_CONNECTIONS,
-                landmark_drawing_spec=mp_drawing.DrawingSpec(
-                    color=(232, 254, 255), thickness=1, circle_radius=4
-                ),
-                connection_drawing_spec=mp_drawing.DrawingSpec(
-                    color=(255, 249, 161), thickness=2, circle_radius=2
-                ),
+            image,
+            landmark_list=results.left_hand_landmarks,
+            connections=mp_holistic.HAND_CONNECTIONS,
+            landmark_drawing_spec=mp_drawing.DrawingSpec(
+                color=(232, 254, 255), thickness=1, circle_radius=4
+            ),
+            connection_drawing_spec=mp_drawing.DrawingSpec(
+                color=(255, 249, 161), thickness=2, circle_radius=2
+            ),
         )
         # Draw right hand connections
         mp_drawing.draw_landmarks(
-                image,
-                landmark_list=results.right_hand_landmarks,
-                connections=mp_holistic.HAND_CONNECTIONS,
-                landmark_drawing_spec=mp_drawing.DrawingSpec(
-                    color=(232, 254, 255), thickness=1, circle_radius=4
-                ),
-                connection_drawing_spec=mp_drawing.DrawingSpec(
-                    color=(255, 249, 161), thickness=2, circle_radius=2
-                ),
+            image,
+            landmark_list=results.right_hand_landmarks,
+            connections=mp_holistic.HAND_CONNECTIONS,
+            landmark_drawing_spec=mp_drawing.DrawingSpec(
+                color=(232, 254, 255), thickness=1, circle_radius=4
+            ),
+            connection_drawing_spec=mp_drawing.DrawingSpec(
+                color=(255, 249, 161), thickness=2, circle_radius=2
+            ),
         )
+        
