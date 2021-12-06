a5b00b456353ea4ab6b8f3caa32b9c6110ed91ec
Gabriel GUERIN <gabriel.guerin@sicara.com>
2021-12-06 16:16:11 +0100

    Create a voting logic to predict the sign

--- utils/dtw.py
+++ utils/dtw.py
@@ -1,9 +1,10 @@
+import pandas as pd
 from fastdtw import fastdtw
 from scipy.spatial.distance import euclidean
 from models.sign_model import SignModel
 
 
-def dtw_distances(recorded_sign: SignModel, sign_dictionary: dict):
+def dtw_distances(recorded_sign: SignModel, sign_dictionary: pd.DataFrame):
     """
     Use DTW to compute similarity between the recorded sign & the reference signs
 
@@ -15,8 +16,10 @@ def dtw_distances(recorded_sign: SignModel, sign_dictionary: dict):
     recorded_left_hand = recorded_sign.lh_embedding
     recorded_right_hand = recorded_sign.rh_embedding
 
-    sign_distances = {}
-    for sign_name, sign_model in sign_dictionary.items():
+    for idx, row in sign_dictionary.iterrows():
+        # Initialize the row variables
+        sign_name, sign_model, _ = row
+
         # If the reference sign has the same number of hands compute fastdtw
         if (recorded_sign.has_left_hand == sign_model.has_left_hand) and (
             recorded_sign.has_right_hand == sign_model.has_right_hand
@@ -24,13 +27,12 @@ def dtw_distances(recorded_sign: SignModel, sign_dictionary: dict):
             sign_left_hand = sign_model.lh_embedding
             sign_right_hand = sign_model.rh_embedding
 
-            sign_distances[sign_name] = 0
             if recorded_sign.has_left_hand:
-                sign_distances[sign_name] += fastdtw(recorded_left_hand, sign_left_hand)[0]
+                row["distance"] += fastdtw(recorded_left_hand, sign_left_hand)[0]
             if recorded_sign.has_right_hand:
-                sign_distances[sign_name] += fastdtw(recorded_right_hand, sign_right_hand)[0]
+                row["distance"] += fastdtw(recorded_right_hand, sign_right_hand)[0]
 
         # If not, distance equals 10000
         else:
-            sign_distances[sign_name] = 10000
-    return dict(sorted(sign_distances.items(), key=lambda item: item[1]))
+            row["distance"] = 10000
+    return sign_dictionary.sort_values(by=["distance"])
